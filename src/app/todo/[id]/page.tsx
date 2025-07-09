9a0ae4bb5e357c8a02f3259597296eb231c80ab9
Leekwanyong <dlrkdid12@gmail.com>
2025-07-09 09:16:44 +0900

    배포 오류 수정

--- src/app/todo/[id]/page.tsx
+++ src/app/todo/[id]/page.tsx
@@ -1,16 +1,17 @@
 import { fetchDetailTodos } from "@/lib/api/todoApi";
 import TodoDetailClient from "./_components/TodoDetailClient";
 
-export async function generateMetadata({ params }: { params: { id: string } }) {
-    const todo = await fetchDetailTodos(Number(params.id));
+export async function generateMetadata({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
+    const todo = await fetchDetailTodos(Number(id));
     return {
         title: `${todo.name} | Codeit TodoList!`,
         description: todo.memo || "할 일 상세 정보",
     };
 }
 
-export default async function TodoDetailPage({ params }: { params: { id: string } }) {
-    const { id } = params;
+export default async function TodoDetailPage({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
     const todo = await fetchDetailTodos(Number(id));
     return <TodoDetailClient initialTodo={todo} />;
 }
